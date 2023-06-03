9535ab7c39dd546ab219960954763a0cafdfb1df
assudani66 <102500920+PiyushAssudani@users.noreply.github.com>
2023-06-04 03:28:50 +0530

    Feat:WishlistedImplemented

--- src/pages/userProfile.js
+++ src/pages/userProfile.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import AddressCard from '../components/AddressCard/AddressCard'
+import { useUserData } from '../context/userContext'
 
 const UserProfile = () => {
+const {getAddresses,removeAddress} = useUserData()
+
+console.log(getAddresses())
   return (
-    <div>UserProfile</div>
+    <div>
+    </div>
   )
 }
 

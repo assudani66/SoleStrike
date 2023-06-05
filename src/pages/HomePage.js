ea67499a96f9f39cbd650cc53800d6b4793a02f5
assudani66 <102500920+PiyushAssudani@users.noreply.github.com>
2023-06-05 09:59:25 +0530

    Feat:newFilters

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useNavigate } from '../../node_modules/react-router-dom/dist/index'
+import { useFilterContext } from '../context/filterContext'
 
 const HomePage = () => {
+  const navigate = useNavigate()
+  const {activeFilters,filterDispatch} = useFilterContext()
   return (
-    <div>HomePage</div>
+    <div>
+      <div onClick={()=>{
+        navigate("/store")
+        filterDispatch({ type: "activeCategory", payload: "Casual_Shoes"})
+        }}>
+        Casual_Shoes
+      </div>
+      <div onClick={()=>{
+        navigate("/store")
+        filterDispatch({ type: "activeCategory", payload: "Casual_Shoes"})
+        }}>
+        Sport_Shoes
+      </div>
+      <div onClick={()=>{
+        navigate("/store")
+        filterDispatch({ type: "activeCategory", payload: "Casual_Shoes"})
+        }}>
+        Casual_Shoes
+      </div>
+      </div>
+    </div>
   )
 }
 

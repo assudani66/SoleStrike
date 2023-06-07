0f7f1ec68cdab3c86ab960939f4b7989f7d1e237
assudani66 <102500920+PiyushAssudani@users.noreply.github.com>
2023-06-07 12:39:58 +0530

    Feat:Razor Pay submission

--- src/pages/checkoutPage/checkOutPage.js
+++ src/pages/checkoutPage/checkOutPage.js
@@ -7,6 +7,33 @@ import "./checkOutPage.css"
 const CheckOutPage = () => {
     const {userData,userDispatch} = useUserData()
     const {cart} = useCart()
+    const handleSubmit = (e)=>{
+        var options = {
+          key: "rzp_test_2pm1kNf4Qwoehw",
+          key_secret:"3xiMRzmvH6iCzETCNeeEMLmT",
+          amount: cart.reduce((arr,curr)=>(arr + curr.price*curr.qty),0)*100,
+          currency:"INR",
+          name:"SOLE_STRIKE",
+          description:"for testing purpose",
+          handler: function(response){
+            alert(response.razorpay_payment_id);
+          },
+          prefill: {
+            name:"adarshbalika",
+            email:"adarshbalika@gmail.com",
+            contact:"7904425033"
+          },
+          notes:{
+            address:"Razorpay Corporate office"
+          },
+          theme: {
+            color:"#3399cc"
+          }
+        };
+        var pay = new window.Razorpay(options);
+        pay.open();
+    }
+  
   return (
     <div className='checkOut'>
     <div>
@@ -21,7 +48,7 @@ const CheckOutPage = () => {
             <p>{price}</p>
         </div>)}
         <p className='order'> total:<span>₹{cart.reduce((arr,curr)=>(arr + curr.price*curr.qty),0)}</span></p>
-      <button>PlaceOrder</button>
+      <button onClick={e=>handleSubmit(e)}>PlaceOrder</button>
       </div>
       
     </div>

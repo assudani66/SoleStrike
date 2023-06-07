0b142917d18eaeb1e0f0a8e15896cf7190d5ab83
assudani66 <102500920+PiyushAssudani@users.noreply.github.com>
2023-06-07 10:24:48 +0530

    Fix:Login Featured

--- src/components/LoginSection/Login.js
+++ src/components/LoginSection/Login.js
@@ -1,25 +1,37 @@
-import React, { useState } from 'react'
-import { useAuth } from '../../context/authContext'
-import { useLocation, useNavigate } from '../../../node_modules/react-router-dom/dist/index'
-
+import React, { useState } from 'react';
+import { useAuth } from '../../context/authContext';
+import { useLocation, useNavigate } from 'react-router-dom';
+import "./Login.css"
 const Login = () => {
-  const {loginInfo,loginUser} =useAuth()
-  const [loginCredentials,setLoginCredentials] = useState({userName:"adarshbalika@gmail.com", userPassword:"adarshbalika"})
-  const navigate = useNavigate()
-  const location = useLocation()
-  const handleLogin = () =>{
-    loginUser(loginCredentials.userName,loginCredentials.userPassword )
-    console.log(location)
-    navigate(location?.state?.from?.pathname)
-  }
-  return (
-    <div>
-      <label>userName<input value={loginCredentials.userName}/></label>
-      <label>password<input value={loginCredentials.userPassword}/></label>
-      <button onClick={()=>handleLogin()}>Login</button>
-      <button onClick={()=>loginUser("adarshbalika@gmail.com", "adarshbalika")}>Login with TestCredits</button>
-    </div>
-  )
-}
+  const { loginInfo, loginUser } = useAuth();
+  const [loginCredentials, setLoginCredentials] = useState({
+    userName: 'adarshbalika@gmail.com',
+    userPassword: 'adarshbalika'
+  });
+  const navigate = useNavigate();
+  const location = useLocation();
+  const handleLogin = () => {
+    loginUser(loginCredentials.userName, loginCredentials.userPassword);
+    console.log(location);
+    navigate(location?.state?.from?.pathname);
+  };
 
-export default Login
+  return (
+    <div className="login-container">
+      <label>
+        userName
+        <input value={loginCredentials.userName} />
+      </label>
+      <label>
+        password
+        <input value={loginCredentials.userPassword} />
+      </label>
+      <button onClick={() => handleLogin()}>Login</button>
+      <button onClick={() => loginUser('adarshbalika@gmail.com', 'adarshbalika')}>
+        Login with TestCredits
+      </button>
+    </div>
+  );
+};
+
+export default Login;
